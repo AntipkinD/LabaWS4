a8d92818d47e113983362ccf1fc633f4b443c970
mushroomvoodoo <normeeboy@gmail.com>
2024-04-14 22:32:06 +0300

    .

--- LabaWS4/Program.cs
+++ LabaWS4/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using StudentSpace;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student[] students = new Student[3] { new Student("Геннадьев", "А.И.", "17ВИ13", 19), new Student("Васильев", "Г.Д.", "11ПА20", 20), new Student() };
+        ReaderSpace.Reader[] readers = new ReaderSpace.Reader[3] { new ReaderSpace.Reader("Геннадьев Алексей Игоревич", 132434, "Всемирная история", "14.09.2004", "+79541234536"), new ReaderSpace.Reader("Васильев Гектор Дмитриевич", 14489, "Палеонтология и археология", "19.08.2004", "+796543256743"), new ReaderSpace.Reader() };
+        for (int i = 0; i < students.Length; i++)
+        {
+            students[i].getInfo(students[i]);
+            readers[i].takeBook("Мастер и Маргарита", "Жизнь насекомых", "Ночной дозор");
+            readers[i].returnBook(3);
+        }
+    }
+}
---
+++ LabaWS4/Student.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSpace
+{
+    internal class Student
+    {
+        string surname;
+        string initials;
+        string numofgroup;
+        private uint age;
+
+        public Student()
+        {
+            this.surname = "undefined";
+            this.initials = "undefined";
+            this.numofgroup = "undefined";
+            this.age = 0;
+        }
+        public Student(string surname, string initials, string numofgroup, uint age)
+        {
+            this.surname = surname;
+            this.initials = initials;
+            this.numofgroup = numofgroup;
+            this.age = age;
+        }
+        
+        public void getInfo(Student student)
+        {
+            Console.WriteLine($"{student.surname}, {this.initials}, {this.numofgroup}, {this.age}");
+        }
+    }
+}
+namespace ReaderSpace
+{
+    internal class Reader
+    {
+        private string fullname;
+        uint librarycardnum;
+        string faculty;
+        private string borndate;
+        string phonenumber;
+
+        public Reader()
+        {
+            this.fullname = "undefined";
+            this.librarycardnum = 0;
+            this.faculty = "undefined";
+            this.borndate = "undefined";
+            this.phonenumber = "undefined";
+        }
+        public Reader(string fullname, uint librarycardnum, string faculty, string borndate, string phonenumber)
+        {
+            this.fullname = fullname;
+            this.librarycardnum = librarycardnum;
+            this.faculty = faculty;
+            this.borndate = borndate;
+            this.phonenumber = phonenumber;
+        }
+
+        public void takeBook(uint booksquantity)
+        {
+            Console.WriteLine($"{this.fullname} взял {booksquantity} книги");
+        }
+        public void takeBook(params string[] books)
+        {
+            string result = " ";
+            foreach (string i in books)
+            {
+                if (i == books[0])
+                    result = $"{this.fullname} взял книги {i}";
+                else
+                    result += $", {i}";
+            }
+            Console.WriteLine(result);
+        }
+        public void returnBook(uint booksquantity)
+        {
+            Console.WriteLine($"{this.fullname} вернул {booksquantity} книги");
+        }
+        public void returnBook(params string[] books)
+        {
+            string result = " ";
+            foreach (string i in books)
+            {
+                if (i == books[0])
+                    result = $"{this.fullname} взял книги {i}";
+                else
+                    result += $", {i}";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
